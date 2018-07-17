741a23c66c7304b99f6d729fbebbcdeb67b39b59
Siddhartha <dev@sdht.in>
2018-07-16 21:27:34 -0400

    Fix

--- 5-convert-to-git.sh
+++ 5-convert-to-git.sh
@@ -22,7 +22,7 @@ for i in */PKGBUILD;do
         -e "/groups=/s/([^a-z0-9\-])kf5([^a-z0-9\-])/\1kf5-opt\2/" \
         -e "/groups=/s/([^a-z0-9\-])plasma([^a-z0-9\-])/\1plasma-opt\2/" \
         -e "/conflicts|provides|replaces/d" \
-        -e "/CMAKE_INSTALL_PREFIX/a\    \-DCMAKE_BUILD_TYPE\=RelWithDebInfo \\\\" \
+        -e "/CMAKE_INSTALL_PREFIX/i\    \-DCMAKE_BUILD_TYPE\=RelWithDebInfo \\\\" \
         -e '/^prepare/i\\npkgver() {\n  cd $_pkgname\n  printf "r%s.%s" "\$\(git rev-list --count HEAD\)" "\$\(git rev-parse --short HEAD\)"\n}\n' \
         -e 's|source=\(.*download.kde.org[^ )]*|source=\(\$\{_pkgname\}::git\+https://anongit.kde.org/\$\{_pkgname\}|' \
         -e '/^build/a\  export PATH="/opt/kde/bin:\$\{PATH\}"\n  export XDG_DATA_DIRS="/opt/kde5/share:\${XDG_DATA_DIRS\}"\n' \

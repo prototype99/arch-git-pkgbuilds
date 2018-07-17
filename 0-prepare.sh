0498a28351ca2ed03769cd48a250fa2a393bd1d2
Siddhartha <dev@sdht.in>
2018-07-16 20:47:49 -0400

    Add kdesrc-build

--- 0-prepare.sh
+++ 0-prepare.sh
@@ -4,3 +4,4 @@ set -e
 
 mkdir -p /home/sdh/builds/{ccm_chroot,logs,old_packages,scripts,sources}
 mkdir -p /home/sdh/builds/pkgbuilds/{arch-packages,working-packages}
+git clone git://anongit.kde.org/kdesrc-build /home/sdh/builds/sources/kdesrc-build

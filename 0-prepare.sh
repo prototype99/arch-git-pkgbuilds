0ed600b7a6ac2c088ff1574d3f39a7b039ec4e57
Siddhartha <dev@sdht.in>
2018-08-19 13:38:25 -0400

    More preparation

--- 0-prepare.sh
+++ 0-prepare.sh
@@ -6,3 +6,5 @@ basedir=/home/sdh/builds
 mkdir -p ${basedir}/{ccm_chroot,logs,old_packages,scripts,sources}
 mkdir -p ${basedir}/pkgbuilds/{arch-packages,working-packages}
 git clone git://anongit.kde.org/kdesrc-build ${basedir}/sources/kdesrc-build
+mkdir -p ${basedir}/sources/kdesrc-build/{s,b,l}
+sudo pacman -S perl-io-socket-ssl perl-yaml-syck perl-xml-parser perl-libwww perl-json

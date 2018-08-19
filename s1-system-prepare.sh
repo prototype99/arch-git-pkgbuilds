c8b4afea9b6f401a4c89f07d24e5a696f466ee13
Siddhartha <dev@sdht.in>
2018-08-19 15:27:07 -0400

    Change shell

--- s1-system-prepare.sh
+++ s1-system-prepare.sh
@@ -1,5 +1,5 @@
 pacman -Syu --needed base-devel git zsh tmux htop glances
-useradd -m sdh
+useradd -m -s /bin/zsh sdh
 passwd sdh
 sed -i -r -e "/^#?Port/cPort 9980" -e "/^#?PermitRootLogin/cPermitRootLogin no" -e "/^#?PasswordAuthentication/cPasswordAuthentication no" /etc/ssh/sshd_config
 echo "Run: ssh-copy-id sdh@"

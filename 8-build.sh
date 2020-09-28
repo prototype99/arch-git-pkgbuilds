db77cb625891528511b8fa05fc945ff96b88fade
Siddhartha <dev@sdht.in>
2020-09-28 00:05:43 -0400

    Fix handlng of local pacman repo

--- 8-build.sh
+++ 8-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-set -e
+set -euo pipefail
+shopt -s nullglob
 
 root="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 builddir=${root}/builds
@@ -16,18 +17,52 @@ case "$1" in
     ;;
 esac
 
-sudo mkdir -p /opt/builds/tmpfs/build && sudo chown kde: $tmpfsbuilddir && sudo pacman -Syu && \
-cd $root/git-pkgbuilds && \
-cp /etc/makepkg.conf $builddir/makepkg.conf && \
+sudo mkdir -p /opt/builds/tmpfs/build
+sudo chown kde: $tmpfsbuilddir
+sudo pacman -Syu
+
+cd $root/git-pkgbuilds
+
+cp /etc/makepkg.conf $builddir/makepkg.conf
 sed -i -r \
     -e '/^CFLAGS=/s/-O2/-O2 -w/' \
     -e '/^CXXFLAGS=/s/-O2/-O2 -w/' \
     -e '/MAKEFLAGS=/s/.*/MAKEFLAGS="-j4"/' \
     -e '/BUILDDIR=/s|.*|BUILDDIR='"$tmpfsbuilddir"'|' \
     -e '/SRCDEST=/s|.*|SRCDEST='"$builddir/sources"'|' \
-    -e '/PACKAGER=/s/.*/PACKAGER="Siddhartha <dev@sdht.in>"/' $builddir/makepkg.conf && \
+    -e '/PACKAGER=/s/.*/PACKAGER="Siddhartha <dev@sdht.in>"/' $builddir/makepkg.conf
+
 for i in $packages;do
     echo "#### $i"
-    bash -c "cd $i-git; makepkg -si --config $builddir/makepkg.conf --noconfirm --needed && . PKGBUILD && ls *.zst | grep -v \$pkgver | xargs -L1 -I{} mv -v {} $builddir/old_packages/ && ls *.tar.zst | xargs -L1 -I{} sudo bash -c '[[ -f $repodir/{} && \"\$(md5sum {} | cut -d\" \" -f1)\" == \"\$(md5sum $repodir/{} | cut -d\" \" -f1)\" ]] && echo \"Already present\" || { cp {} $repodir && repo-add -R -p $repodir/local-repo.db.tar.gz $repodir/{}; }'" || break
+    cd $i-git
+
+    # Add repo if present and not already added
+    if [[ -f $repodir/local-repo.db.tar.gz ]] && ! grep local-repo /etc/pacman.conf &>/dev/null;then
+        sudo sed -i -e '$a[local-repo]\nSigLevel = Never\nServer = file://'"$repodir"'\n' /etc/pacman.conf && \
+        sudo pacman -Syu
+    fi
+
+    # Run makepkg
+    if makepkg -s --config $builddir/makepkg.conf --noconfirm --needed;then
+        true
+    else
+        # Exit unless the error is "package already present"
+        [[ "$?" != "13" ]] && exit 1
+    fi
+    
+    . PKGBUILD
+    # Remove old versions
+    ls *.zst | grep -v $pkgver | xargs -L1 -I{} mv -v {} $builddir/old_packages/ || true
+    # Cleanup tmpfs to avoid OOM
+    rm -rf $tmpfsbuilddir/$i-git || true
+    # Add new packages to pacman repo if not already present
+    for of in *.tar.zst;do
+        if [[ -f $repodir/$of && "$(md5sum $of | cut -d" " -f1)" == "$(md5sum $repodir/$of | cut -d" " -f1)" ]];then
+            echo "Already present"
+        else
+            cp $of $repodir && repo-add -R -p $repodir/local-repo.db.tar.gz $repodir/$of && sudo pacman -Syu
+        fi
+    done
+    cd ..
 done
 
